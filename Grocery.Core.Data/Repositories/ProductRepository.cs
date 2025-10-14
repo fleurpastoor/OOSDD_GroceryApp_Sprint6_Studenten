a8e97ab2d166a1a1def00ee65e53e1e09b4051b9
Fleur <s1212464@student.windesheim.nl>
2025-10-14 10:25:29 +0200

    UC19: ProductRepo compleet via database
    - Delete en Update gebeurt nu ook via de database

--- Grocery.Core.Data/Repositories/ProductRepository.cs
+++ Grocery.Core.Data/Repositories/ProductRepository.cs
@@ -10,23 +10,23 @@ namespace Grocery.Core.Data.Repositories
         private readonly List<Product> products = [];
         public ProductRepository()
         {
-            CreateTable(@"CREATE TABLE IF NOT EXISTS Products (
+            CreateTable(@"CREATE TABLE IF NOT EXISTS Product (
                             [Id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                             [Name] NVARCHAR(80) UNIQUE NOT NULL,
                             [Stock] INTEGER NOT NULL,
                             [Date] DATE NOT NULL,
                             [Price] DECIMAL NOT NULL)"); 
-            List<string> insertQueries = [@"INSERT OR IGNORE INTO Products(Name, Stock, Date, Price) VALUES('Melk', 100, '2025-09-25', 0.95)",
-                                          @"INSERT OR IGNORE INTO Products(Name, Stock, Date, Price) VALUES('Kaas', 50, '2025-09-30', 7.98)",
-                                          @"INSERT OR IGNORE INTO Products(Name, Stock, Date, Price) VALUES('Brood', 200, '2025-09-12', 2.19)",
-                                          @"INSERT OR IGNORE INTO Products(Name, Stock, Date, Price) VALUES('Cornflakes', 0, '2025-12-31', 1.48)"];
+            List<string> insertQueries = [@"INSERT OR IGNORE INTO Product(Name, Stock, Date, Price) VALUES('Melk', 100, '2025-09-25', 0.95)",
+                                          @"INSERT OR IGNORE INTO Product(Name, Stock, Date, Price) VALUES('Kaas', 50, '2025-09-30', 7.98)",
+                                          @"INSERT OR IGNORE INTO Product(Name, Stock, Date, Price) VALUES('Brood', 200, '2025-09-12', 2.19)",
+                                          @"INSERT OR IGNORE INTO Product(Name, Stock, Date, Price) VALUES('Cornflakes', 0, '2025-12-31', 1.48)"];
             InsertMultipleWithTransaction(insertQueries);
             GetAll();
         }
         public List<Product> GetAll()
         {
             products.Clear();
-            string selectQuery = "SELECT Id, Name, Stock, date(Date), Price FROM Products";
+            string selectQuery = "SELECT Id, Name, Stock, date(Date), Price FROM Product";
             OpenConnection();
             using (SqliteCommand command = new(selectQuery, Connection))
             {
@@ -48,7 +48,7 @@ namespace Grocery.Core.Data.Repositories
 
         public Product? Get(int id)
         {
-            string selectQuery = $"SELECT Id, Name, Stock, date(Date), Price FROM Products WHERE Id = {id}";
+            string selectQuery = $"SELECT Id, Name, Stock, date(Date), Price FROM Product WHERE Id = {id}";
             Product? p = null;
             OpenConnection();
             using (SqliteCommand command = new(selectQuery, Connection))
@@ -67,7 +67,6 @@ namespace Grocery.Core.Data.Repositories
             }
             CloseConnection();
             return p;
-            return products.FirstOrDefault(p => p.Id == id);
         }
 
         public Product Add(Product item)
@@ -91,15 +90,29 @@ namespace Grocery.Core.Data.Repositories
 
         public Product? Delete(Product item)
         {
-            throw new NotImplementedException();
+            string deleteQuery = $"DELETE FROM Product WHERE Id = {item.Id};";
+            OpenConnection();
+            Connection.ExecuteNonQuery(deleteQuery);
+            CloseConnection();
+            return item;
         }
 
         public Product? Update(Product item)
         {
-            Product? product = products.FirstOrDefault(p => p.Id == item.Id);
-            if (product == null) return null;
-            product.Id = item.Id;
-            return product;
+            int recordsAffected;
+            string updateQuery = $"UPDATE Product SET Name = @Name, Stock = @Stock, Date = @Date, Price = @Price  WHERE Id = {item.Id};";
+            OpenConnection();
+            using (SqliteCommand command = new(updateQuery, Connection))
+            {
+                command.Parameters.AddWithValue("Name", item.Name);
+                command.Parameters.AddWithValue("Stock", item.Stock);
+                command.Parameters.AddWithValue("Date", item.ShelfLife);
+                command.Parameters.AddWithValue("Price", item.Price);
+
+                recordsAffected = command.ExecuteNonQuery();
+            }
+            CloseConnection();
+            return item;
         }
     }
 }
